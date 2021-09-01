4ea482e614bc4f25feca6623b4bb02139e53c7c4
Stiventh <Stiventh@DESKTOP-6U92HUV>
2021-09-01 15:47:04 -0500

    Add Entety

--- todoAF.Function/Entities/TodoEntity.cs
+++ todoAF.Function/Entities/TodoEntity.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.WindowsAzure.Storage.Table;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace todoAF.Function.Entities
 {
-    public class TodoEntity: TableEntity
+    public class TodoEntity : TableEntity
     {
         public int Idemployee { get; set; }
         public DateTime WorkingHour { get; set; }
